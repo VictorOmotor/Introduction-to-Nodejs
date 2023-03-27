180a1be78d6e9398a7935053ebc6adcc89cdac81
omotorvidosky <omotorvidosky@gmail.com>
2023-03-27 10:35:20 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const track = {
+    name: "Victor Omotor",
+    prefLang: "Nodejs",
+    futurePref: "Over the next one (I) year, I see myself working as a valuable team player in the Fintech industry as a Backend Engineer.",
+}
+
+console.log("My name is " + track.name + ", and my preferred language is "  + track.prefLang + ". " + track.futurePref)
